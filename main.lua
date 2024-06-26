b616ebab46ea7efe4dcb60514911aa753c2d9197
Hanaka15 <34461088+Hanaka15@users.noreply.github.com>
2024-06-26 13:30:20 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,14 +1,16 @@
+-- Do "main.js true" if you want force updates.
+
 -- Define the GitHub link
 local github = "https://raw.githubusercontent.com/Hanaka15/CCJukebox/main/"
 
 -- Function to update the script
 local function update()
     -- Download the updated script
-    shell.run("wget", github .. "jukebox.lua")
+    shell.run("wget -qO /dev/null ", github .. "jukebox.lua")
     
     -- get the startup file.
     if not fs.exists("startup.lua") then
-        shell.run("wget", github .. "startup.lua")
+        shell.run("wget -qO /dev/null ", github .. "startup.lua")
     else
         print("Startup file already exists...")
     end
@@ -33,13 +35,16 @@ if version_file then
     version_file:close()
     
     -- Get the version info from GitHub
-    local version_github = http.get(github .. "version.txt")
+    local version_github = http.get(github .. "version.txt", {protocol = "https"})
     if version_github then
         local version_info = version_github.readAll()
         version_github.close()
         
-        -- Compare local version with GitHub version
-        if version_local ~= version_info then
+        -- Check if the arg is "true"
+        local force_update = select(1, ...) == "true"
+        
+        -- Compare local version with GitHub version or force update
+        if force_update or version_local ~= version_info then
             update()
         else
             print("Script is up to date")
